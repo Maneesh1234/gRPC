1d4ef1e7bed0fd5d0e653b9eca4ba0e20b4c9be3
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2021-01-28 23:28:36 +0530

    client

---
+++ Ustaph_Sample/greet_4/greet_client/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"log"
+
+	"gRpc/Udemy/greet_4_20/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+// SEE LINE 305 IN GREET.PB.GO
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "MANEESH",
+			LastName:  "KUMAR",
+		},
+	}
+	// SEE LINE 307 IN GREET.PB.GO
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
+}
+
+func main() {
+
+	fmt.Println("Hello I'm a client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	//fmt.Printf("Created client: %f", c)
+
+	doUnary(c)
+
+}
