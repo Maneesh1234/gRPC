484b8ad815bfccfa891bc5074547173aca329a35
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2021-02-24 22:35:05 +0530

    SSL Security

---
+++ Ustaph_Sample/greet_s9_ssl/greet_s9_ssl/greet_server/server.go
@@ -0,0 +1,153 @@
+package main
+
+// FOR RUNNING SERVER
+// CD UDEMY
+//cd "greet_s9_ssl"
+//go run greet_server\server.go
+
+////COMMAND FOR CODE GENERATION
+//protoc greet_s9_ssl/greetpb/greet.proto --go_out=plugins=grpc:.
+
+import (
+	"context"
+	"fmt"
+	"gRpc/Udemy/greet_s9_ssl/greetpb"
+	"google.golang.org/grpc/credentials"
+	"io"
+	"log"
+	"net"
+	"strconv"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+// SEE LINE 330 IN GREET.PB.GO
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	// SEE LINE 68 IN GREET.PB.GO
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
+// SEE LINE 505 IN GREET.PB.GO
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("GreetManyTimes function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManytimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
+}
+
+// SEE LINE 691 IN GREET.PB.GO
+func (*server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
+	// IN THIS STREAM HAVE stream.Recv() AND stream.SendAndClose( METHOD
+
+	fmt.Printf("LongGreet function was invoked with a client streaming request\n")
+	result := ""
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// SEE LINE 743 IN GREET.PB.GO
+			// WE HAVE TO FINISH THE CLIENT STREAM
+			return stream.SendAndClose(&greetpb.LongGreetResponse{
+				Result: result,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+
+		firstName := req.GetGreeting().GetFirstName()
+		result += "Hello " + firstName + "! "
+	}
+
+}
+
+// SEE LINE 849 IN GREET.PB.GO
+func (*server) GreetEveryone(stream greetpb.GreetService_GreetEveryoneServer) error {
+	// IN THIS STREAM HAVE stream.Recv() AND stream.Send( METHOD
+	fmt.Printf("GreetEveryone function was invoked with a streaming request\n")
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+			return err
+		}
+		firstName := req.GetGreeting().GetFirstName()
+		result := "Hello " + firstName + "! "
+
+		sendErr := stream.Send(&greetpb.GreetEveryoneResponse{
+			Result: result,
+		})
+		if sendErr != nil {
+			log.Fatalf("Error while sending data to client: %v", sendErr)
+			return sendErr
+		}
+	}
+
+}
+
+func main() {
+	fmt.Println("Hello world")
+
+	//tcp connection
+	// 50051 is the default port number for grpc
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// //(1) WAY
+
+	// certFile := "ssl/server.crt"
+	// keyFile := "ssl/server.pem"
+	// creds, sslErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+	// if sslErr != nil {
+	// 	log.Fatalf("Failed loading certificates: %v", sslErr)
+	// 	return
+	// }
+	// opts := grpc.Creds(creds)
+
+	// s := grpc.NewServer(opts)
+
+	//(2) WAY
+	opts := []grpc.ServerOption{}
+	tls := true
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, sslErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if sslErr != nil {
+			log.Fatalf("Failed loading certificates: %v", sslErr)
+			return
+		}
+
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	s := grpc.NewServer(opts...)
+
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	//BIND THE PORT NUMBER  TO THE GRPC SERVER
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
