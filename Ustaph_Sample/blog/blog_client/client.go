74d14589cf3dfcabd6012499ccc84da1bd635b42
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2021-03-03 18:25:20 +0530

    Creating blog with grpc and mongodb

---
+++ Ustaph_Sample/blog/blog_client/client.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"gRpc/Udemy/blog/blogpb"
+	
+	"log"
+	
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	fmt.Println("Blog Client")
+
+	opts := grpc.WithInsecure()
+
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	// create Blog
+	fmt.Println("Creating the blog")
+	blog := &blogpb.Blog{
+		AuthorId: "Tom",
+		Title:    "My Second Blog",
+		Content:  "Content of the Second blog",
+	}
+	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+	fmt.Printf("Blog has been created: %v", createBlogRes)
+	blogID := createBlogRes.GetBlog().GetId()
+
+	
+	
+}
