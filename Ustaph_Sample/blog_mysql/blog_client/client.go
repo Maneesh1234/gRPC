5c474049e29751ba08ec05e741214d118bbbf0a4
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2021-03-18 17:59:33 +0530

    read operation with grpc and mysql

--- Ustaph_Sample/blog_mysql/blog_client/client.go
+++ Ustaph_Sample/blog_mysql/blog_client/client.go
@@ -37,7 +37,24 @@ func main() {
 		log.Fatalf("Unexpected error: %v", err)
 	}
 	fmt.Printf("Blog has been created: %v", createBlogRes)
-	
+	authorId := createBlogRes.GetBlog().GetAuthorId()
+
+	// read Blog
+	fmt.Println("Reading the blog")
+
+	_, err2 := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{AuthorId: 34})
+	if err2 != nil {
+		fmt.Printf("Error happened while reading: %v \n", err2)
+	}
+
+	readBlogReq := &blogpb.ReadBlogRequest{AuthorId: authorId}
+	readBlogRes, readBlogErr := c.ReadBlog(context.Background(), readBlogReq)
+	if readBlogErr != nil {
+		fmt.Printf("Error happened while reading: %v \n", readBlogErr)
+	}
+
+	fmt.Printf("Blog was read: %v \n", readBlogRes)
+
 	
 
 }
